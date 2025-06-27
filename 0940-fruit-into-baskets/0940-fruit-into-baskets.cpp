be6f862f99053aacf899a78737fe3db3199d6d13
Shivam Verma <128091749+Shivam995698@users.noreply.github.com>
2025-06-27 14:44:19 +0530

    Time: 39 ms (62.84%), Space: 75 MB (76.07%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,82 +1,52 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        // unordered_set<int>st;
-        
         // int n = fruits.size();
-        // int maxlen =0;
+        
+        // int maxlen = INT_MIN;
         // for(int i =0;i<n;i++)
         // {
-        //     unordered_set<int>st;
+        //     set<int>st;
         //     for(int j =i;j<n;j++)
         //     {
         //         st.insert(fruits[j]);
         //         if(st.size()<=2)
         //         {
-        //             int len = j-i+1;
-        //             maxlen = max(len,maxlen);
+        //             maxlen = max(maxlen,j-i+1);
+        //         }
+        //         else{
+        //             break;
         //         }
         //     }
         // }
         // return maxlen;
 
-
-
-        //approch 2
-        // int left =0;
-        // int right=0;
-        // int maxlen =0;
-        // unordered_map<int,int>mp;
-        // int n = fruits.size();
-        // while(right<n)
-        // {
-        //     mp[fruits[right]]++;
-        //     if(mp.size()>2)
-        //     {
-        //         while(mp.size()>2)
-        //         {
-        //             mp[fruits[left]]--;
-        //             if(mp[fruits[left]]==0)
-        //             {
-        //                 mp.erase(fruits[left]);
-        //             }
-        //             left++;
-        //         }
-        //     }
-        //     if(mp.size()<=2)
-        //     {
-        //         int len = right-left+1;
-        //         maxlen = max(len,maxlen);
-        //     }
-        //     right++;
-        // }
-        // return maxlen;
-
-
-        //approch 3
         int left = 0;
         int right = 0;
         int maxlen = 0;
+        unordered_map<int,int>mp;
         int n = fruits.size();
-        map<int,int>mp;
         while(right<n)
         {
             mp[fruits[right]]++;
             if(mp.size()>2)
             {
-                mp[fruits[left]]--;
-                if(mp[fruits[left]]==0)
+                while(mp.size()>2)
                 {
-                    mp.erase(fruits[left]);
-                    
+                    mp[fruits[left]]--;
+                    if(mp[fruits[left]]==0)
+                    {
+                        mp.erase(fruits[left]);
+                    }
+                    left++;
                 }
-                left++;
-                
+
             }
-            if(mp.size()<=2)
-            {
-                maxlen = max(maxlen,right-left+1);
-            }
+                if(mp.size()<=2)
+                {
+                    int len = right-left+1;
+                    maxlen = max(maxlen,len);
+                }
             right++;
         }
         return maxlen;
