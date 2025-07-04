a4f88bc41df5fedfa1b3935a3f63c6483870f6df
Shivam Verma <128091749+Shivam995698@users.noreply.github.com>
2025-07-04 16:09:45 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.58%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+        int index = -1;
+        //finding the breaking point 
+        for(int i = n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                index = i;
+                break;
+            }
+        }
+        if(index==-1)
+        {
+            sort(nums.begin(),nums.end());
+        }
+        else{
+                //finding the next greater element breakPoint 
+                for(int i = n-1;i>index;i--)
+                {
+                    if(nums[i]>nums[index])
+                    {
+                        swap(nums[index],nums[i]);
+                        break;
+                    }
+                }
+                //sort the remaing array 
+                sort(nums.begin()+index+1,nums.end());
+            }
     }
 };
